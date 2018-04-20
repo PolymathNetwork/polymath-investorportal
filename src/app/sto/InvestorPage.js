321772ee69b790fa7ed54f3a3c26cc5f5161a53d
Shannon Clarke <shannonjamalclarke@gmail.com>
2018-04-20 01:56:42 -0400

    Fixed with updated polymathJS (using current contracts) and updated polymath UI

--- src/app/sto/InvestorPage.js
+++ src/app/sto/InvestorPage.js
@@ -4,7 +4,6 @@ import DocumentTitle from 'react-document-title'
 import { Icon } from 'carbon-components-react'
 import { connect } from 'react-redux'
 import { STOStatus } from 'polymath-ui'
-import BigNumber from 'bignumber.js'
 import PropTypes from 'prop-types'
 import { change } from 'redux-form'
 // import type { SecurityToken, STOPurchase, STODetails } from 'polymathjs'
@@ -12,17 +11,17 @@ import { change } from 'redux-form'
 // import NotFoundPage from '../../NotFoundPage'
 // import InvestorTable from './components/InvestorTable'
 import BuyTokenForm, { formName } from './components/BuyTokenForm'
-import { getTicker, fetch } from './actions'
+import { fetch } from './actions'
 import type { RootState } from '../../redux/reducer'
 
 type StateProps = {|
-  stage: number,
-|}
+  stage: number
+|};
 
 type DispatchProps = {|
-  change: (? string) => any,
-    fetch: () => any,
-|}
+  change: (?string) => any,
+  fetch: () => any
+|};
 
 // type Props = StateProps & DispatchProps
 
@@ -36,50 +35,43 @@ const mapStateToProps = (state: RootState): StateProps => ({
 const mapDispatchToProps: DispatchProps = {
   change: (value) => change(formName, 'owner', value, false, false),
   fetch,
-  getTicker,
 }
 
 class InvestorPage extends Component {
-
   static propTypes = {
-    // balance: PropTypes.string,
-    // account: PropTypes.string,
-    getTicker: PropTypes.func.isRequired,
+    fetch: PropTypes.func.isRequired,
     change: PropTypes.func.isRequired,
-    // match: PropTypes.func,
-    // details: PropTypes.Array,
-    // investors: PropTypes.Array,
     account: PropTypes.string.isRequired,
     token: PropTypes.Object,
+    details: PropTypes.Object,
     purchases: PropTypes.Array,
-  }
+  };
 
   componentWillMount () {
-    // this.props.fetch()
-    this.props.getTicker("SHTO")
+    this.props.fetch('TORO')
+    // this.props.getTicker('TORO')
     this.props.change(this.props.account)
   }
 
   render () {
     // console.log(this.props)
-    const { token } = this.props
+    const { token, details } = this.props
 
-    const details = {
-      start: new Date(2018, 1, 30),
-      end: new Date(2018, 6, 30),
-      raised: new BigNumber(5000),
-      cap: new BigNumber(2000000),
-      isPolyFundraise: false,
-    }
+    // const details = {
+    //   start: new Date(2018, 1, 30),
+    //   end: new Date(2018, 6, 30),
+    //   raised: new BigNumber(5000),
+    //   cap: new BigNumber(2000000),
+    //   isPolyFundraise: false,
+    // }
 
     return (
       <DocumentTitle title='Polymath'>
         <div className='bx--row'>
-
           <div className='bx--col-xs-12'>
-            {this.props.token ? (
+            {this.props.token && details ? (
               <div>
-
+                {/* <p>{token.owner}</p> */}
                 <STOStatus
                   title={token.name}
                   start={details.start}
@@ -90,31 +82,31 @@ class InvestorPage extends Component {
                 />
                 <p>&nbsp;</p>
               </div>
-            ) : 'Loading...'}
-
+            ) : (
+              'Loading...'
+            )}
           </div>
           <div className='bx--col-xs-6'>
-
             <BuyTokenForm title='Buy Tokens' />
-
           </div>
           <div className='bx--col-xs-6'>
-
-            <h3 className='bx--type-beta'><Icon
-              name='info--glyph'
-              fill='blue'
-              description='This is a description of the icon and what it does…'
-              className='extra-class'
-            />&nbsp;&nbsp;Instruction Title for Investor
+            <h3 className='bx--type-beta'>
+              <Icon
+                name='info--glyph'
+                fill='blue'
+                description='This is a description of the icon and what it does…'
+                className='extra-class'
+              />&nbsp;&nbsp;Instruction Title for Investor
             </h3>
             <p className='bx--type-gamma'>What to do Next:</p>
             <p className='bx--type-gamma'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat
             </p>
           </div>
         </div>
-
       </DocumentTitle>
     )
   }
--- src/app/sto/actions.js
+++ src/app/sto/actions.js
@@ -6,7 +6,7 @@ import type { STOFactory, STODetails, STOPurchase } from 'polymathjs/types'
 
 // import { formName as configureFormName } from './components/ConfigureSTOForm'
 import type { ExtractReturn } from '../../redux/helpers'
-import type { GetState } from '../../redux/reducer'
+// import type { GetState } from '../../redux/reducer'
 // import { actionGen } from '../../redux/helpers'
 
 // export const DEMO = 'demo'
@@ -16,40 +16,90 @@ export const TICKER = 'sto/TICKER'
 export const ticker = (token) => ({ type: TICKER, token })
 
 export const DATA = 'sto/DATA'
-export const data = (contract: STO, details: ?STODetails) => ({ type: DATA, contract, details })
+export const data = (contract: STO, details: ?STODetails) => ({
+  type: DATA,
+  contract,
+  details,
+})
 
 export const FACTORIES = 'sto/FACTORIES'
-export const factories = (factories: Array<STOFactory>) => ({ type: FACTORIES, factories })
+export const factories = (factories: Array<STOFactory>) => ({
+  type: FACTORIES,
+  factories,
+})
 
 export const USE_FACTORY = 'sto/USE_FACTORY'
-export const useFactory = (factory: STOFactory) => ({ type: USE_FACTORY, factory })
+export const useFactory = (factory: STOFactory) => ({
+  type: USE_FACTORY,
+  factory,
+})
 
 export const PURCHASES = 'sto/PURCHASES'
-export const purchases = (purchases: Array<STOPurchase>) => ({ type: PURCHASES, purchases })
+export const purchases = (purchases: Array<STOPurchase>) => ({
+  type: PURCHASES,
+  purchases,
+})
 
 export type Action =
   | ExtractReturn<typeof data>
-  | ExtractReturn<typeof factories>
+  | ExtractReturn<typeof factories>;
 
 export const getTicker = (tickerName) => async (dispatch) => {
-
   var tickerinfo = await SecurityTokenRegistry.getTokenByTicker(tickerName)
+  // console.log(tickerinfo)
+
+  // tickerinfo.contract.setSTO()
+
+  // var balance = await PolyToken.myBalance()
+  // console.log("my Balance", balance)
 
   dispatch(ticker(tickerinfo))
-
 }
 
-export const fetch = () => async (dispatch: Function, getState: GetState) => {
+export const fetch = (tickerName) => async (dispatch: Function) => {
   dispatch(ui.fetching())
   try {
-    const token = getState().token.token
-    if (!token || !token.contract) {
+    // const token = getState().ticker.token
+    const token = await SecurityTokenRegistry.getTokenByTicker(tickerName)
+
+    if (!token) {
+      // console.log('No token found')
+      return dispatch(ui.fetched())
+    } else {
+      // console.log('token', token)
+      dispatch(ticker(token))
+    }
+
+    if (!token.contract) {
+      // console.log('No contract found')
       return dispatch(ui.fetched())
     }
+
     const sto = await token.contract.getSTO()
 
-    dispatch(data(sto, sto ? await sto.getDetails() : null))
-    dispatch(ui.fetched())
+    var stoFactory = await CappedSTOFactory.address
+    // console.log(stoFactory)
+
+    // const sto = await tickerinfo.contract.getSTO()
+    // console.log(sto)
+    // console.log(token.contract.account)
+    // let tokenDetails = null
+    if (sto === null) {
+      await token.contract.setSTO(
+        stoFactory,
+        new Date(2018, 5, 7),
+        new Date(2018, 11, 31),
+        '1000000',
+        '500',
+        false,
+        token.contract.account
+      )
+    } else {
+      // tokenDetails = await sto.getDetails()
+      // console.log(tokenDetails)
+      dispatch(data(sto, sto ? await sto.getDetails() : null))
+      dispatch(ui.fetched())
+    }
   } catch (e) {
     dispatch(ui.fetchingFailed(e))
   }
@@ -60,21 +110,32 @@ export const fetchFactories = () => async (dispatch: Function) => {
   dispatch(ui.fetching())
   try {
     const title = await CappedSTOFactory.getTitle()
-    dispatch(factories([{
-      title,
-      name: 'Polymath Inc.',
-      usedBy: ['The Bureau Shevchenko LLC', 'Everhusk Inc.', 'Koverko & Co'],
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.,',
-      isVerified: true,
-      securityAuditBy: 'Zeppelin Solutions',
-      address: CappedSTOFactory.address,
-    }]))
+    dispatch(
+      factories([
+        {
+          title,
+          name: 'Polymath Inc.',
+          usedBy: [
+            'The Bureau Shevchenko LLC',
+            'Everhusk Inc.',
+            'Koverko & Co',
+          ],
+          desc:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' +
+            'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' +
+            'laboris nisi ut aliquip ex ea commodo consequat.,',
+          isVerified: true,
+          securityAuditBy: 'Zeppelin Solutions',
+          address: CappedSTOFactory.address,
+        },
+      ])
+    )
   } catch (e) {
     dispatch(ui.fetchingFailed(e))
   }
 }
 
-/*export const configure = () => async (dispatch: Function, getState: GetState) => {
+/* export const configure = () => async (dispatch: Function, getState: GetState) => {
   try {
     const factory = getState().sto.factory
     if (!factory) {
@@ -109,16 +170,19 @@ export const fetchFactories = () => async (dispatch: Function) => {
   }
 }*/
 
-export const fetchPurchases = () => async (dispatch: Function, getState: GetState) => {
-  dispatch(ui.fetching())
-  try {
-    const contract = getState().sto.contract
-    if (!contract) {
-      return
-    }
-    dispatch(factories(await contract.getPurchases()))
-    dispatch(ui.fetched())
-  } catch (e) {
-    dispatch(ui.fetchingFailed(e))
-  }
-}
+// export const fetchPurchases = () => async (
+//   dispatch: Function,
+//   getState: GetState
+// ) => {
+//   dispatch(ui.fetching())
+//   try {
+//     const contract = getState().sto.contract
+//     if (!contract) {
+//       return
+//     }
+//     dispatch(factories(await contract.getPurchases()))
+//     dispatch(ui.fetched())
+//   } catch (e) {
+//     dispatch(ui.fetchingFailed(e))
+//   }
+// }
